7dc046f3ebd8f5a6d8d08b555fef57f50d4d356c
seydametova <seidametovaf1@gmail.com>
2021-09-19 22:17:06 +0200

    hw-1

---
+++ hw-1/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <calc/>
+  </div>
+</template>
+
+<script>
+import Calc from './components/Calc.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calc
+  },
+  data: () => ({
+    message: 'Привет, VUE',
+    show: true,
+    c: 0,
+    a: 1,
+    b: 1,
+    msg: 'Welcome to Your Vue.js App'
+  }),
+  methods: {
+    doThat () {
+      this.c = 0
+    },
+    blurMethod () {
+      console.log('blur')
+    },
+    focusMethod () {
+      console.log('focus')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
